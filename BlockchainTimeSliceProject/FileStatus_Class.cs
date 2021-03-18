bd054e3cfc61f85dd188a3de965b3786cf33e0f0
2228754688 <2228754688@qq.com>
2021-03-18 08:45:14 +0800

    update try

---
+++ BlockchainTimeSliceProject/FileStatus_Class.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockchainTimeSliceProject
+{
+    class FileStatus_Class
+    {
+        string fileName { get; }
+        int totalBlocks { get; }
+        int unusedBlocks { get; set; }
+        bool readStatus = false;
+        int[] blockStatusArray { get; set; }
+
+        public FileStatus_Class() { 
+        }
+
+        public FileStatus_Class(string fileName,int totalBlocks) {
+            this.fileName = fileName;
+            this.totalBlocks = totalBlocks;
+            this.unusedBlocks = 0;
+            this.readStatus = true;
+            this.blockStatusArray = new int[this.totalBlocks];
+        }
+    }
+}
